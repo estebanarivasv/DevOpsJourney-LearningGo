e92e6dc71ea1ca28e36320f1d6be35d59afd503a
Tebi <estebanarivasv@gmail.com>
2024-12-03 09:17:22 -0300

    feat: add intro, str vars and operators

---
+++ 3_strings_basics/1_intro_strings.py
@@ -0,0 +1,12 @@
+'''
+Strings: they are textual data types. 
+- We can see them as strings of characters.
+- Another basic data type in Python (separate from numerical floats and integers).
+- Can be represented in double or single quotes.
+    - You need to be consistent --> You cannot open with single and close 
+                                    with double quotes.
+- i.e: color = "Magenta" // color = 'Magenta'
+'''
+print(type("anything")) # <class 'str'>
+print(type("7")) # <class 'str'>
+print(type(7)) # <class 'int'>
---
+++ 3_strings_basics/2_string_variables.py
@@ -0,0 +1,8 @@
+'''
+String variables behaviour: 
+- Python does not care what we put in a variable
+'''
+age = 85
+print(type(age), age) # <class 'int'> 85
+age = "eighty-five"
+print(type(age), age) # <class 'str'> eighty-five
